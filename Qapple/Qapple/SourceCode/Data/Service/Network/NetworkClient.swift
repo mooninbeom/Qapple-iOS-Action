144f68301c09dca28bd18851f1ff3215a7ad52a3
omj0722 <omj0722@icloud.com>
2025-01-21 21:28:42 +0900

    [#256] NetworkClient 메서드 추가

--- Qapple/Qapple/SourceCode/Data/Service/Network/NetworkClient.swift
+++ Qapple/Qapple/SourceCode/Data/Service/Network/NetworkClient.swift
@@ -18,26 +18,77 @@ protocol HTTPMethod {
 /// 네트워킹을 수행할 클라이언트 객체
 struct NetworkClient {
     
-    /// GET 요청을 수행합니다.
-    ///
-    /// - 사전과제 내 네트워크 요청은 GET만 존재하므로 해당 함수만 구현
-    func get<T>(url: URL) async throws -> T where T: Decodable {
+    /// SignIn 요청을 수행합니다.
+    func signIn<T: Decodable, U: Encodable>(url: URL, body: U) async throws -> T {
         do {
             
-            // 1. 네트워킹 수행
-            let (data, response) = try await request(from: url)
+            // 네트워킹 수행(토큰 X)
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
             
-            // 2. StatusCode 체크
-            let statusCode = statusCode(response)
-            try checkStatusCode(statusCode: statusCode)
+            request.httpBody = try JSONEncoder().encode(body)
             
-            // 3. Decoding 및 반환
-            let decodedData: T = try decoding(to: data)
-            return decodedData
+            let (data, response) = try await URLSession.shared.data(for: request)
+            
+            return try decodeResponse(data: data, response: response)
         } catch {
             throw error
         }
     }
+    
+    /// GET 요청을 수행합니다.
+    func get<T>(url: URL) async throws -> T where T: Decodable {
+        do {
+            
+            // 네트워킹 수행
+            let (data, response) = try await request(url: url, method: "GET")
+            
+            return try decodeResponse(data: data, response: response)
+        } catch {
+            throw error
+        }
+    }
+    
+    /// POST 요청을 수행합니다.
+    func post<T: Decodable, U: Encodable>(url: URL, body: U) async throws -> T {
+        do {
+            
+            // 네트워킹 수행
+            let (data, response) = try await request(url: url, body: body, method: "POST")
+            
+            return try decodeResponse(data: data, response: response)
+        } catch {
+            throw error
+        }
+    }
+    
+    /// FETCH 요청을 수행합니다.
+    func fetch<T: Decodable, U: Encodable>(url: URL, body: U) async throws -> T {
+        do {
+            
+            // 네트워킹 수행
+            let (data, response) = try await request(url: url, body: body, method: "FETCH")
+            
+            return try decodeResponse(data: data, response: response)
+        } catch {
+            throw error
+        }
+    }
+    
+    /// DELETE 요청을 수행합니다.
+    func delete<T>(url: URL) async throws -> T where T: Decodable {
+        do {
+            
+            // 네트워킹 수행
+            let (data, response) = try await request(url: url, method: "DELETE")
+            
+            return try decodeResponse(data: data, response: response)
+        } catch {
+            throw error
+        }
+    }
+    
 }
 
 // MARK: - URLSession
@@ -47,14 +98,21 @@ extension NetworkClient {
     /// URLSession을 통한 네트워킹을 수행합니다.
     ///
     /// - 커스텀 에러(NetworkError)를 반환하기 위해, 재사용을 염두에 두고 함수로 감싸주었습니다.
-    func request(from url: URL) async throws -> (Data, URLResponse) {
+    func request(url: URL, body: Encodable? = nil, method: String) async throws -> (Data, URLResponse) {
         do {
             var request = URLRequest(url: url)
-            request.httpMethod = "GET"
-            request.setValue(
+            request.httpMethod = method
+            request.setValue( // TODO: 실제 토큰으로 변경
                 "Bearer eyJhbGciOiJIUzUxMiJ9.eyJ0b2tlblR5cGUiOiJhY2Nlc3MiLCJtZW1iZXJJZCI6MTU0LCJyb2xlIjoiQUNBREVNSUVSIiwiaWF0IjoxNzM2OTE1ODMyLCJleHAiOjE3Mzc3Nzk4MzJ9.x2qe-8kZ3wyl0JGsNOfCmKoioEXoZS9H5liwG2A1bWDgeJk3JPkPPKm86Hbu5B_VD1UK5nJ_JxwQm1wTen04Hg",
                 forHTTPHeaderField: "Authorization"
             )
+            
+            // Body가 있는 경우 Content-Type 추가 및 Body 인코딩
+            if let body = body {
+                request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+                request.httpBody = try JSONEncoder().encode(body)
+            }
+            
             return try await URLSession.shared.data(for: request)
         } catch {
             throw NetworkError2.urlRequestFailure(urlString: url.absoluteString)
@@ -103,7 +161,19 @@ extension NetworkClient {
             throw NetworkError2.decodingFailure(data: data)
         }
     }
+    
+    /// 응답을 검증하고 데이터를 디코딩한 결과를 반환합니다.
+    func decodeResponse<T: Decodable>(data: Data, response: URLResponse) throws -> T {
+        // StatusCode 체크
+        let statusCode = statusCode(response)
+        try checkStatusCode(statusCode: statusCode)
+        
+        // Decoding 및 반환
+        return try decoding(to: data)
+    }
 }
+
+// MARK: - Network Error
 
 /// 네트워킹에서 발생할 수 있는 에러를 정의합니다.
 enum NetworkError2: Error {
