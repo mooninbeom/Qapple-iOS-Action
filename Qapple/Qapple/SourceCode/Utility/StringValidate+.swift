d6a8b849e8ddbfecb4ec6050be2d0a6d039a1815
thinkyside <eunlyuing@gmail.com>
2025-02-11 20:17:34 +0900

    [#298] 특수기호 및 공백 확인 로직 수정

--- Qapple/Qapple/SourceCode/Utility/StringValidate+.swift
+++ Qapple/Qapple/SourceCode/Utility/StringValidate+.swift
@@ -11,7 +11,7 @@ extension String {
     
     /// 특수 기호 체크
     var checkSpecialChar: Bool {
-        let pattern = "^[가-힣a-zA-Z\\s]*$"
+        let pattern = "^[가-힣a-zA-Z]*$"
         if let regex = try? NSRegularExpression(pattern: pattern, options: .caseInsensitive) {
             let range = NSRange(location: 0, length: self.utf16.count)
             if regex.firstMatch(in: self, options: [], range: range) != nil {
