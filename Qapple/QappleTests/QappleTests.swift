f4f2651cd132262af9c64a780913d9556cf4994f
mooninbeom <dlsqja567@naver.com>
2025-02-21 21:15:20 +0900

    UnitTest 추가

---
+++ Qapple/QappleTests/QappleTests.swift
@@ -0,0 +1,17 @@
+//
+//  QappleTests.swift
+//  QappleTests
+//
+//  Created by 문인범 on 2/21/25.
+//
+
+import Testing
+@testable import Qapple
+
+
+struct QappleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+}
