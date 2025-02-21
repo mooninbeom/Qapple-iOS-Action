f723dc2ef5efa5824bac985587fdf798eeba81d2
thinkyside <eunlyuing@gmail.com>
2025-02-21 21:28:38 +0900

    [#313] Legacy API 호출 코드 삭제

---
+++ Qapple/Qapple/SourceCode/Entity/QappleAPI.swift
@@ -0,0 +1,18 @@
+//
+//  QappleAPI.swift
+//  Qapple
+//
+//  Created by 김민준 on 2/21/25.
+//
+
+import Foundation
+
+enum QappleAPI {
+    
+    typealias TotalCount = Int
+    
+    struct PaginationInfo: Equatable {
+        var threshold: String
+        var hasNext: Bool
+    }
+}
