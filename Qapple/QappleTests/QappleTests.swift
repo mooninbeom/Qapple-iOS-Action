5047029e6e4bd9f44d594b02661ced319baea95d
mooninbeom <dlsqja567@naver.com>
2025-02-27 15:45:55 +0900

    타겟 추가

---
+++ Qapple/QappleTests/QappleTests.swift
@@ -0,0 +1,16 @@
+//
+//  QappleTests.swift
+//  QappleTests
+//
+//  Created by 문인범 on 2/27/25.
+//
+
+import Testing
+
+struct QappleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
